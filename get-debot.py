7df49b6b7f4b4fb62571dbdb9af5e4d8ebe47634
abhinand4858 <abhinand4858@gmail.com>
2018-03-02 16:34:29 +0530

    Get screen_name from debot

---
+++ get-debot.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+slist = []
+
+for i in range(10,20):
+    res = requests.post('http://cs.unm.edu/~chavoshi/debot/api.php', data={'api_key':'uSTTnDjq7DmKgaYpBAklP4wBsIKAiHfDp0mEtxUq', 'srv_type': '3','date_1': '2018-01-'+i, 'date_2': '2018-01-'+i})
+
+    res = BeautifulSoup(res.text, "lxml")
+    sname = res.response.day.findAll("screen_name") #will have the list of <screen_name>abc</screen_name>
+
+    for i in sname:
+        slist.append(str(i)[13:-14])
+
+    print slist
+    print "done"
+
+
+
+
+	
